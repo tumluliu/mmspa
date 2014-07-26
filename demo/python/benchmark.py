f1026aec0253e298213a42f452991146e36fc0b5
tumluliu <nudtlliu@gmail.com>
2014-07-27 01:08:49 +0800

    add stub files for python bindings

---
+++ demo/python/benchmark.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from pymmspa import PathFinder
+
+source_list = []
+mode_list = ['underground', 'foot']
+
+def do_benchmarking(algorithm):
+    # Timer clock start 
+    for s in source_list:
+        if algorithm == 'MMTQ':
+            PathFinder.multimodal_twoq(s, mode_list)
+        elif algorithm == 'MMD':
+            PathFinder.multimodal_dijkstra(s, mode_list)
+        elif algorithm == 'MMBF':
+            PathFinder.multimodal_bellmanford(s, mode_list)
+    # Timer clock stop
+    print "Calculation time: "
+
+if __name__ == "__main__":
+    do_benchmarking(arg1)
+
+
