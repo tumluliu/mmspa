8ebf8241bb9249a5807cf7a326daeee1c3d9aaae
tumluliu <nudtlliu@gmail.com>
2014-07-28 02:22:07 +0800

    modifiy source file to pymmspa_module.c

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup, Extension
 
-MMSPA_MODULE = Extension('pymmspa',
-                         include_dirs=['/usr/local/include'],
-                         libraries=['mmspa'],
-                         library_dirs=['/usr/local/lib'],
-                         sources=['pymmspa.c'])
+PYMMSPA_MODULE = Extension('pymmspa',
+                           include_dirs=['/usr/local/include'],
+                           libraries=['mmspa'],
+                           library_dirs=['/usr/local/lib'],
+                           sources=['pymmspa_module.c'])
 
 setup(name='pymmspa',
-      version=1.0,
+      version='0.1',
       description='Python binding of multimodal shortest path algorithms',
       author='Lu LIU',
       author_email='nudtlliu@gmail.com',
-      ext_modules=[MMSPA_MODULE])
+      ext_modules=[PYMMSPA_MODULE])
