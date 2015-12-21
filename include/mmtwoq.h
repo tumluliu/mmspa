e2bc7c70f2db0dd1c047a756a0e0265fdaf0d680
tumluliu <nudtlliu@gmail.com>
2015-12-21 23:48:31 +0800

    a standalone header for mmtq algorithm

---
+++ include/mmtwoq.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mmtwoq.h
+ *
+ *    Description:  Interface of multimodal Two-Q algorithm
+ *
+ *        Created:  2015/12/21 22时59分19秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Lu LIU (lliu), nudtlliu@gmail.com
+ *   Organization:  LfK@TUM
+ *
+ * =====================================================================================
+ */
+
+#ifndef  MMTWOQ_INC
+#define  MMTWOQ_INC
+
+#include "routingplan.h"
+#include "routingresult.h"
+
+void MultimodalTwoQ(int64_t source);
+
+#endif   /* ----- #ifndef MMTWOQ_INC  ----- */
