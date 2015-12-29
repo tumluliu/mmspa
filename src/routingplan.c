3b6f56a515c220395fde691d24b01bdd7f2092c5
tumluliu <nudtlliu@gmail.com>
2015-12-29 17:36:07 +0800

    v2.x API definitions of routingplan

--- src/routingplan.c
+++ src/routingplan.c
@@ -19,7 +19,7 @@
 
 RoutingPlan *plan = NULL;
 
-void CreateRoutingPlan(int modeCount, int publicModeCount) {
+void MSPcreateRoutingPlan(int modeCount, int publicModeCount) {
 	plan = (RoutingPlan*) malloc(sizeof(RoutingPlan));
 	plan->mode_count = modeCount;
 	plan->public_transit_mode_count = publicModeCount;
@@ -41,28 +41,57 @@ void CreateRoutingPlan(int modeCount, int publicModeCount) {
 		        sizeof(int));
 }
 
-void SetModeListItem(int index, int modeId) {
+void MSPsetMode(int index, int modeId) {
 	plan->mode_id_list[index] = modeId;
 }
 
-void SetPublicTransitModeSetItem(int index, int modeId) {
+void MSPsetPublicTransit(int index, int modeId) {
 	plan->public_transit_mode_id_set[index] = modeId;
 }
 
-void SetSwitchConditionListItem(int index, const char *spCondition) {
+void MSPsetSwitchCondition(int index, const char *spCondition) {
 	plan->switch_condition_list[index] = spCondition;
 }
 
-void SetSwitchingConstraint(int index, VertexValidationChecker callback) {
+void MSPsetSwitchingConstraint(int index, VertexValidationChecker callback) {
 	plan->switch_constraint_list[index] = callback;
 }
 
-void SetTargetConstraint(VertexValidationChecker callback) {
+void MSPsetTargetConstraint(VertexValidationChecker callback) {
 	plan->target_constraint = callback;
 }
 
-void SetCostFactor(const char *costFactor) {
+void MSPsetCostFactor(const char *costFactor) {
 	plan->cost_factor = costFactor;
+}
+
+/* v1.x API, for compatibility */
+void CreateRoutingPlan(int modeCount, int publicModeCount) {
+    MSPcreateRoutingPlan(modeCount, publicModeCount);
+}
+
+void SetModeListItem(int index, int modeId) {
+	MSPsetMode(index, modeId);
+}
+
+void SetPublicTransitModeSetItem(int index, int modeId) {
+	MSPsetPublicTransit(index, modeId);
+}
+
+void SetSwitchConditionListItem(int index, const char *spCondition) {
+	MSPsetSwitchCondition(index, spCondition);
+}
+
+void SetSwitchingConstraint(int index, VertexValidationChecker callback) {
+	MSPsetSwitchingConstraint(index, callback);
+}
+
+void SetTargetConstraint(VertexValidationChecker callback) {
+	MSPsetTargetConstraint(callback);
+}
+
+void SetCostFactor(const char *costFactor) {
+	MSPsetCostFactor(costFactor);
 }
 
 void DisposeRoutingPlan() {
