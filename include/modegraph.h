49c293be914c354b1ca20db0cc66ca168d54ea33
tumluliu <nudtlliu@gmail.com>
2015-12-31 15:15:28 +0800

    use structure pointers in the typedefs

--- include/modegraph.h
+++ include/modegraph.h
@@ -22,19 +22,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define VNULL (Vertex*)NULL
-#define ENULL (Edge*)NULL
-#define GNULL (ModeGraph*)NULL
+#define VNULL (Vertex)NULL
+#define ENULL (Edge)NULL
+#define GNULL (ModeGraph)NULL
 #define VERY_FAR 1073741823
 /* TOTAL_MODES is 7 instead of 9 because so far the TAXI mode is not considered, 
  * and the public transport is actually a abstract mode consisting of UNDERGROUND, 
  * SUBURBAN, TRAM and BUS */
 #define TOTAL_MODES 7
 
-typedef struct Edge Edge;
-typedef struct Vertex Vertex;
-typedef struct ModeGraph ModeGraph;
-typedef struct SwitchPoint SwitchPoint;
+typedef struct Edge *Edge;
+typedef struct Vertex *Vertex;
+typedef struct ModeGraph *ModeGraph;
+typedef struct SwitchPoint *SwitchPoint;
 typedef enum MODES MODES;
 
 /* Must be consistent everywhere in the whole multimodal routing application,
@@ -73,7 +73,7 @@ struct Edge
 	double  speed_factor;
 	int64_t from_vertex_id;
 	int64_t to_vertex_id;
-	Edge    *adjNext;
+	Edge    adjNext;
 };
 
 struct Vertex
@@ -81,25 +81,24 @@ struct Vertex
 	int64_t id; /* unique id */
 	double  temp_cost; /* tentative distance value */
 	double  distance;
-	double  elapsed_time;
+	double  duration;
 	double  walking_distance;
 	double  walking_time;
 	/* TODO: how about other types of cost along the path, e.g. gas, energy,
 	 * battery, fatigue, etc.? Here the ideal way is to make such metrics
 	 * configurable. */
-	Vertex  *parent; /* successor vertex in the SPT */
+	Vertex  parent; /* successor vertex in the SPT */
 	int     outdegree;
-	Edge    *outgoing; /* first outgoing edge in adjacency edge list */
+	Edge    outgoing; /* first outgoing edge in adjacency edge list */
 	int     status;
-	Vertex  *next; /* next vertex in queue */
+	Vertex  next; /* next vertex in queue */
 };
 
 struct ModeGraph
 {
 	int    id; /* mode id of graph */
-	Vertex **vertices; /* vertex set */
+	Vertex *vertices; /* vertex list */
 	int    vertex_count; /* number of vertices */
-	int    edge_count; /* number of edges */
 };
 
 struct SwitchPoint
@@ -111,7 +110,7 @@ struct SwitchPoint
 	double  speed_factor;
 };
 
-Vertex *SearchVertexById(Vertex **vertexArray, int len, int64_t id);
-Vertex *BinarySearchVertexById(Vertex **vertexArray, int low, int high, int64_t id);
+Vertex SearchVertexById(Vertex *vertexArray, int len, int64_t id);
+Vertex BinarySearchVertexById(Vertex *vertexArray, int low, int high, int64_t id);
 
 #endif /* MODEGRAPH_H_ */
